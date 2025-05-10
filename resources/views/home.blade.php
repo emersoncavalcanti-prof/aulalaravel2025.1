10e00ddba6c067049e2363f38d88fb771cbcb7e3
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2025-05-09 21:07:51 -0300

    add bootstrap icon ao projeto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,11 @@
         <title>Laravel</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     </head>
     <body>
 
-    <button class="btn btn-primary">Enviar</button>
+    <button class="btn btn-primary"><i class="bi bi-filetype-php"></i> Enviar</button>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     </body>
