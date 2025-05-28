243d2207c6354f163c9a038ab348d4da7647a9a9
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2025-05-28 20:16:34 -0300

    criando componente para cadastro

---
+++ app/Livewire/Cadastro.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Cadastro extends Component
+{
+    public function cadastrar()
+    {
+        
+    }
+    public function render()
+    {
+        return view('livewire.cadastro');
+    }
+}
--- resources/views/cadastro.blade.php
+++ resources/views/cadastro.blade.php
@@ -1,40 +1,5 @@
- @extends('auth')
+@extends('auth')
  
- @section('content')
- <div class="row justify-content-center align-items-center min-vh-100">
-            <div class="col-md-6 col-lg-4">
-                <div class="card shadow">
-                    <div class="card-body">
-                        <h3 class="card-title text-center mb-4">Cadastro</h3>
-        
-                        <!-- Formulário de login -->
-                        <form method="POST" action="/login">
-                            @csrf
-
-                            <div class="mb-3">
-                                <label for="email" class="form-label">Nome</label>
-                                <input type="email" name="email" class="form-control" id="email" required autofocus>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="email" class="form-label">E-mail</label>
-                                <input type="email" name="email" class="form-control" id="email" required autofocus>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="password" class="form-label">Senha</label>
-                                <input type="password" name="password" class="form-control" id="password" required>
-                            </div>
-
-                            <div class="d-grid">
-                                <button type="submit" class="btn btn-success mb-1">Cadastrar</button>
-                            </div>
-                            
-                        </form>
-
-                      
-                    </div>
-                </div>
-            </div>
-        </div>
+@section('content')
+@livewire('cadastro') 
 @endsection
