be35eab9f3efaf393f65b216b009eb99b66eac5c
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2025-05-28 21:01:45 -0300

    cadastro de usuarios finalizado

--- app/Livewire/Cadastro.php
+++ app/Livewire/Cadastro.php
@@ -2,13 +2,32 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class Cadastro extends Component
 {
+    public $name;
+    public $email;
+    public $password;
+
     public function cadastrar()
     {
-        
+        $this->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+
+        $this->reset();
+        $this->redirect('/login');
     }
     public function render()
     {
--- resources/views/livewire/cadastro.blade.php
+++ resources/views/livewire/cadastro.blade.php
@@ -5,22 +5,25 @@
                         <h3 class="card-title text-center mb-4">Cadastro</h3>
         
                         <!-- Formulário de login -->
-                        <form wire:click="cadastrar">
+                        <form wire:submit="cadastrar">
                             @csrf
 
                             <div class="mb-3">
-                                <label for="email" class="form-label">Nome</label>
-                                <input type="email" name="email" class="form-control" id="email" required autofocus>
+                                <label for="name" class="form-label">Nome</label>
+                                <input wire:model="name" type="text" name="name" class="form-control" id="name" autofocus>
+                                @error('name') <span class="text text-danger">{{$message}}</span> @enderror
                             </div>
 
                             <div class="mb-3">
                                 <label for="email" class="form-label">E-mail</label>
-                                <input type="email" name="email" class="form-control" id="email" required autofocus>
+                                <input type="email" wire:model="email" name="email" class="form-control" id="email">
+                                @error('email') <span class="text text-danger">{{$message}}</span> @enderror
                             </div>
 
                             <div class="mb-3">
                                 <label for="password" class="form-label">Senha</label>
-                                <input type="password" name="password" class="form-control" id="password" required>
+                                <input type="password" wire:model="password" name="password" class="form-control" id="password">
+                                 @error('password') <span class="text text-danger">{{$message}}</span> @enderror
                             </div>
 
                             <div class="d-grid">
