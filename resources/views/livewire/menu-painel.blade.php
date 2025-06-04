88f5d37c9b9192498729b4a9f96620e8f0c5d95e
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2025-06-04 20:08:49 -0300

    criando menu offcanvas

--- resources/views/livewire/menu-painel.blade.php
+++ resources/views/livewire/menu-painel.blade.php
@@ -1,7 +1,7 @@
 <div>
   <nav class="navbar navbar-expand-lg bg-primary border-bottom border-body mb-5" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="{{route('home')}}"><i class="bi bi-cast"></i> Dashboard</a>
+    <a class="navbar-brand" href="#" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample"><i class="bi bi-list"></i> Dashboard</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -14,5 +14,22 @@
 
     </div>
   </div>
+  
 </nav>
+
+
+
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Menu</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+
+      <ul class="">
+        <li class="list-group-item"><a href="#" class="text text-secondary text-decoration-none"><i class="bi bi-person"></i> Cadastro de Alunos</li></a>
+      </ul>
+
+  </div>
+</div>
 </div>
