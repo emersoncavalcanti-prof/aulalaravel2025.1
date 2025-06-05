9145dfd4d42cccaadc0d018501b233d0781b2434
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2025-06-04 21:17:31 -0300

    criando variaveis no componente do cadastroAlunos

--- app/Livewire/CadastroAlunos.php
+++ app/Livewire/CadastroAlunos.php
@@ -6,6 +6,12 @@ use Livewire\Component;
 
 class CadastroAlunos extends Component
 {
+
+    public $nome;
+    public $telefone;
+    public $email;
+    public $cep;
+
     public function render()
     {
         return view('livewire.cadastro-alunos');
